4879fdbbd5dba50344dc2cd15cd7ef3d94d486eb
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-25 07:34:36 +0000

    About variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,9 @@
+const userId = 23443;
+let userName = "Alex";
+var userState = "Active";
+
+let userLogDate;                //This is undefined value of userLogDate.
+
+userPassword = 23252234;        // we can declare variable without let,const and var.
+
+console.table([userId, userName, userState, userLogDate, userPassword]);
