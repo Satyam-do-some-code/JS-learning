584041c306c90589a023edcee45559dd34cdbdfd
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-25 09:20:49 +0000

    datatypes in js

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,16 @@
+let num = 34;              //JavaScript numbers are always stored as double precision floating point numbers.  2 to power 53
+                           //BigInt is used for bigger number values.It is used in like Stock market.
+
+let name = "sam";          // Everything that is written in Doubles or Single qoutes ("",'') is String.
+
+let user_logged = true;    //Gives the values in either in True or False.
+
+let userId ;               //The variable is created but the value didn't putted yet.
+
+let address = null;        //The Null type has exactly one value, called null.
+                           //Symbol unique identifier.
+
+
+console.table([typeof num, name, user_logged, userId, address]);
+
+console.log(typeof null);
