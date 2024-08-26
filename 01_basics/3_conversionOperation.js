c664230f5539d8aca36e244137ff74151916412b
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-26 07:08:57 +0000

    coversion of datatype

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -1,6 +1,40 @@
-let num = "hello"+23;
+// let num = "39grbiug";
 
-// num = String(num);
+// num = Number(num);
 
-console.log(typeof num);
-console.log( num);
+// console.log(typeof num);
+// console.log(num);
+
+
+
+
+let str = "29abc";
+
+str = Number(str);
+
+console.log(str);
+console.log(typeof str);
+
+
+// Converion to number 
+/* "33" = 33
+    true = 1
+    false = 0
+    "234abc" = NaN // Not a Number */
+
+let userLoggedIn = 1;
+
+userLoggedIn = Boolean(userLoggedIn);
+
+console.log(userLoggedIn);
+
+
+//Conversion to boolean
+/*
+   0 = false
+   1 = true
+  emptyString "" = false
+  filledString "hello" = true 
+
+
+*/
