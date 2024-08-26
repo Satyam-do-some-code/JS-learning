5abc8bf889ac38ff6e1a3cb2aeb31bfd123e3302
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-26 11:07:14 +0000

    comparision, null with comparision and undefined.

---
+++ 01_basics/4_comparision.js
@@ -0,0 +1,19 @@
+
+// console.log("2" > 1);    //true
+// console.log(1 < "0");    //false
+
+
+console.log(null > 0);      // false
+
+
+
+console.log(null == 0);             /*  If null is compared with anything else (like a number, string, object, etc.), it will 
+                                     always return false.
+                                     
+                                    When comparing null with another value using ==, JavaScript only considers null equal to undefined. It does not coerce null to any other value.   */
+
+
+console.log(null >= 0);    //true
+
+
+console.log(undefined >= 0);     //false
