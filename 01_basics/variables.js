c2367ebea11e85fe04f88edee203183a387fac27
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-27 07:54:47 +0000

    about variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,9 +1,24 @@
-const userId = 23443;
-let userName = "Alex";
-var userState = "Active";
+// const userId = 23443;
+// let userName = "Alex";
+// var userState = "Active";
 
-let userLogDate;                //This is undefined value of userLogDate.
+// let userLogDate;                //This is undefined value of userLogDate.
 
-userPassword = 23252234;        // we can declare variable without let,const and var.
+// userPassword = 23252234;        // we can declare variable without let,const and var.
 
-console.table([userId, userName, userState, userLogDate, userPassword]);
+// console.table([userId, userName, userState, userLogDate, userPassword]);
+
+
+
+let name = "Patty";
+
+console.log(name);
+
+console.log(typeof name);
+
+
+let dummy = Number(name);
+
+console.log(dummy);
+
+console.log(typeof dummy);
