789221ee0810b793b5643666154ef477294c9e5a
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-27 16:30:50 +0000

    some more about strings

--- 01_basics/6_string.js
+++ 01_basics/6_string.js
@@ -1,10 +1,33 @@
 //       String interpolation
 
-let name = "Micky";
-let age = 24;
+// let name = "Micky";
+// let age = 24;
 
-console.log(`Hello my name is ${name} and my age is ${age}.`);
+// console.log(`Hello my name is ${name} and my age is ${age}.`);
 
 
 
+
+const user = new String("Richard");
+
+// console.log(user);
+// console.log(typeof user);
+
+// console.log(user[5]);
+
+
+// console.log(user.indexOf('d'));     //This method tells the index number of the string.
+
+
+// console.log(user.charAt(5));     //This method tells that at position string is according to index number.
+
+
+const newString = user.substring(0,4);      // method is use to know the starting to end of the string.
+
+// console.log(newString);
+
+
+const anotherString = user.slice(-6,4); 
+
+console.log(anotherString);
 
