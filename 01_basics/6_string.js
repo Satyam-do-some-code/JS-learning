1b5caedd532fa8cc4369a336feafcc4b533f7326
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-28 08:07:03 +0000

    some more about strings

--- 01_basics/6_string.js
+++ 01_basics/6_string.js
@@ -2,7 +2,6 @@
 
 // let name = "Micky";
 // let age = 24;
-
 // console.log(`Hello my name is ${name} and my age is ${age}.`);
 
 
@@ -12,22 +11,36 @@ const user = new String("Richard");
 
 // console.log(user);
 // console.log(typeof user);
-
 // console.log(user[5]);
 
 
 // console.log(user.indexOf('d'));     //This method tells the index number of the string.
-
-
 // console.log(user.charAt(5));     //This method tells that at position string is according to index number.
 
 
-const newString = user.substring(0,4);      // method is use to know the starting to end of the string.
+// const newString = user.substring(0,4);      // method is use to know the starting to end of the string.
+// // console.log(newString);
 
-// console.log(newString);
+
+// const anotherString = user.slice(-6,4); 
+// console.log(anotherString);
 
 
-const anotherString = user.slice(-6,4); 
 
-console.log(anotherString);
+// const text = "     hello    ";
+// console.log(text)
+// console.log(text.trim());              //Removes starting and ending space from string.
+
+
+// const newText = "user123";
+// console.log(newText);
+// console.log(newText.replace('123' , '345'));        //replace the string.
+
+
+
+const Word = "apple-banana-orange-papaya-pineaple"
+const z = Word.split('-');                          //split method is use to split the strings and convert them into "Array".
+ console.log(z[0]);
+
+
 
