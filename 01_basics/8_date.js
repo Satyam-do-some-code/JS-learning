436ba6a02809e3b7213551035635301394b75491
Satyam-do-some-code <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-08-28 17:14:30 +0000

    knowing about date and time in js

---
+++ 01_basics/8_date.js
@@ -0,0 +1,7 @@
+// Date
+
+const mydate = new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString(undefined,));     // This will return date and time.
