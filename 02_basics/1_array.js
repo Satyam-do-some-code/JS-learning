abec1a26ec1592a223f2f18fafdb854a0a7e724a
Satyaprakash Sharma <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-09-03 13:03:40 +0000

    array depth

---
+++ 02_basics/1_array.js
@@ -0,0 +1,11 @@
+const arr = [];
+
+arr.push("apple","banana","orange");      //".push()" method is use to add elements in an Array
+console.log(arr);                          
+
+const myarr = arr.slice(0,1);             //".slice()" using this we can take out the copy of the elemenst from an array.
+console.log(myarr);
+
+const myarr_1 = arr.splice(2,2);           //".splice()" using this we can take out the elemenst from an array,and taken out elements will be deleted from array.
+console.log(myarr_1);
+console.log(arr);
