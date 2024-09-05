8a6edb8f9e4985398acfff1d0ac1c8f29b292af8
Satyaprakash Sharma <143353503+Satyam-do-some-code@users.noreply.github.com>
2024-09-05 18:06:09 +0000

    some more methods of array

--- 02_basics/1_array.js
+++ 02_basics/1_array.js
@@ -1,11 +1,26 @@
 const arr = [];
 
 arr.push("apple","banana","orange");      //".push()" method is use to add elements in an Array
-console.log(arr);                          
+// console.log(arr);                          
 
 const myarr = arr.slice(0,1);             //".slice()" using this we can take out the copy of the elemenst from an array.
-console.log(myarr);
+// console.log(myarr);
 
 const myarr_1 = arr.splice(2,2);           //".splice()" using this we can take out the elemenst from an array,and taken out elements will be deleted from array.
-console.log(myarr_1);
+// console.log(myarr_1);
+// console.log(arr);
+
+
+arr.pop();
+
+console.log(arr);
+
+arr.push("mango","kiwi","strawberry");
+console.log(arr);
+
+
+arr.shift();
+console.log(arr);
+
+arr.unshift("lichi");
 console.log(arr);
